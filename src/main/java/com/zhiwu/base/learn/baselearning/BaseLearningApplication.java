b9627c348807d3b6c8bc8340465955772e39541f
zhiwuli0228 <132126949+zhiwuli0228@users.noreply.github.com>
2024-06-28 21:47:15 +0800

    Initial commit

---
+++ src/main/java/com/zhiwu/base/learn/baselearning/BaseLearningApplication.java
@@ -0,0 +1,13 @@
+package com.zhiwu.base.learn.baselearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhiwu/base/learn/baselearning/BaseLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhiwu.base.learn.baselearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
